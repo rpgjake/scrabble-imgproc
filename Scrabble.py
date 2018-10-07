9a59a930b131131396a958b125eb1f443cb52932
Stephen Wilkins <wilkins.c.stephen@gmail.com>
2018-10-07 18:25:08 -0400

    Initial working version of edge detect

--- Scrabble.py
+++ Scrabble.py
@@ -2,8 +2,10 @@ import dippykit as dip
 import numpy as np
 import skimage.morphology as morph
 import scipy.ndimage as ndImage
-I1 = dip.im_read('scrabble/image1.jpeg')
-I1 = dip.rgb2gray(I1)
+from skimage.util.dtype import dtype_limits
+image = dip.im_read('images/Board3.png')
+print(image.shape)
+image = dip.rgb2gray(image[:, :, 0:3])
 
 #print(I1.shape)
 conn = np.array([[0, 0, 0, 1, 1, 1, 0, 0, 0],
@@ -15,24 +17,33 @@ conn = np.array([[0, 0, 0, 1, 1, 1, 0, 0, 0],
                  [0, 1, 1, 1, 1, 1, 1, 1, 0],
                  [0, 0, 1, 1, 1, 1, 1, 0, 0],
                  [0, 0, 0, 1, 1, 1, 0, 0, 0]])
+conn = np.array([[1, 1],
+                 [1, 1]])
 
+levels = [("Canny", .8),
+          ("Dilate", conn),
+          ("Gauss", .1)
+          ]
 
-Is_1 = dip.edge_detect(I1, 'canny', True, sigma=1)
-#Is_P5 = dip.edge_detect(I1, 'canny', True, sigma=0.5)
-Dilated = morph.dilation(Is_1, selem=conn)
-Dilated = ndImage.gaussian_filter(Dilated, .25)
-Dilated = morph.dilation(Dilated, selem=conn)
-Dilated = ndImage.gaussian_filter(Dilated, .25)
-Dilated = morph.dilation(Dilated, selem=conn)
-Gauss = ndImage.gaussian_filter(Dilated, 2)
-Labeled, numobj = ndImage.label(Gauss)
-# dip.figure('Dilated')
-# dip.imshow(Dilated, 'gray')
-# dip.figure('Gauss')
-# dip.imshow(Gauss, 'gray')
-# dip.show()
-dip.figure('Label')
-dip.imshow(Labeled, 'gray')
+for i in range(0, len(levels)):
+    trans, param = levels[i]
+    if trans == "Canny":
+        dtype_max = dtype_limits(image, clip_negative=False)[1]
+        low_threshold = 0.2 * dtype_max
+        high_threshold = 0.8 * dtype_max
+        image = dip.edge_detect(image, 'canny', True, sigma=param, low_threshold=low_threshold, high_threshold=high_threshold)
+    elif trans == "Dilate":
+        image = morph.dilation(image, selem=param)
+    elif trans == "Gauss":
+        image = ndImage.gaussian_filter(image, param)
+    text = trans + ":  " + str(i)
+    if trans != "Dilate":
+        text +=  "      " + str(param)
+    dip.figure(text)
+    dip.imshow(image, 'gray')
+    dip.show()
+
+Labeled, numobj = ndImage.label(image)
 lastSum = 0
 displayImage = None
 for item in range(1, numobj + 1):
@@ -46,3 +57,7 @@ for item in range(1, numobj + 1):
 dip.figure("Largest Blob")
 dip.imshow(displayImage, 'gray')
 dip.show()
+
+
+
+
