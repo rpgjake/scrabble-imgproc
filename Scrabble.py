e42fc5784601d3a824c9adeac68969fc5ae6a66c
Stephen Wilkins <wilkins.c.stephen@gmail.com>
2018-11-20 18:07:38 -0500

    Enhanced edge detection by using blue channel only

--- Scrabble.py
+++ Scrabble.py
@@ -3,13 +3,10 @@ import numpy as np
 import skimage.morphology as morph
 import scipy.ndimage as ndImage
 import skimage.transform as tf
-
 import cv2
 
-
-
-image = dip.im_read('images/Words10.jpg')
-image = dip.rgb2gray(image[:, :, 0:3])
+image = dip.im_read('images/Words6.jpg')
+image = (image[:, :, 2])
 
 x, y = image.shape
 if max(x, y) > 1000:
@@ -31,15 +28,15 @@ original = image
 #                  [0, 0, 0, 1, 1, 1, 0, 0, 0]])
 # conn = np.array([[1, 1],
 #                  [1, 1]])
-# conn = np.array([[0, 1, 0],
-#                  [1, 1, 1],
-#                  [0, 1, 0]])
-conn = np.array([[0, 0, 1, 1, 0, 0],
-                 [0, 0, 1, 1, 0, 0],
-                 [1, 1, 1, 1, 1, 1],
-                 [1, 1, 1, 1, 1, 1],
-                 [0, 0, 1, 1, 0, 0],
-                 [0, 0, 1, 1, 0, 0]])
+conn = np.array([[0, 1, 0],
+                 [1, 1, 1],
+                 [0, 1, 0]])
+# conn = np.array([[0, 0, 1, 1, 0, 0],
+#                  [0, 0, 1, 1, 0, 0],
+#                  [1, 1, 1, 1, 1, 1],
+#                  [1, 1, 1, 1, 1, 1],
+#                  [0, 0, 1, 1, 0, 0],
+#                  [0, 0, 1, 1, 0, 0]])
 
 
 
@@ -100,16 +97,16 @@ print("TR:  " + str(cornerTR))
 print("BL:  " + str(cornerBL))
 print("BR:  " + str(cornerBR))
 dest = np.array([cornerTL, cornerBL, cornerBR, cornerTR])
-scale = 15 * 15 * 20
+scale = 1000 # 15 * 15 * 20
 src = np.array([[0, 0], [0, scale], [scale, scale], [scale, 0]])
 tform3 = tf.ProjectiveTransform()
 tform3.estimate(src, dest)
 warped = tf.warp(original, tform3, output_shape=[scale, scale])
-warped = warped[150 : -150, 150 : -150]
+warped = warped[30 : -30, 30 : -30]
 
-# dip.figure("warped")
-# dip.imshow(warped, 'gray')
-# dip.show()
+dip.figure("warped")
+dip.imshow(warped, 'gray')
+dip.show()
 
 
 warped = dip.float_to_im(warped)
@@ -119,23 +116,27 @@ lines = lsd.detect(warped)[0]
 for line in lines:
     for (x1, y1, x2, y2) in line:
         length = abs(np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
-        cv2.line(image3, (x1, y1), (x2, y2), 255, 1)
-image3 = morph.dilation(image3, selem=conn)
-#
-# Labeled, numobj = ndImage.label(image3)
-# lastSum = 0
-# displayImage = None
-# for item in range(1, numobj + 1):
-#     newImage = (Labeled == item)
-#     newSum = newImage.sum()
-#     if newSum > lastSum:
-#         displayImage = newImage
-#         lastSum = newSum
+        cv2.line(image3, (x1, y1), (x2, y2), 255, 3)
+# image3 = morph.dilation(image3, selem=conn)
 
+
+Labeled, numobj = ndImage.label(image3)
+lastSum = 0
+displayImage = None
+testImage = None
+for item in range(1, numobj + 1):
+    newImage = (Labeled == item)
+    newSum = newImage.sum()
+    if newSum > lastSum:
+        displayImage = newImage
+        lastSum = newSum
+
+dip.figure("Display image")
+dip.imshow(displayImage, 'gray')
+dip.show()
 
 
 
 dip.figure("Image 3")
 dip.imshow(image3, 'gray')
 dip.show()
-
