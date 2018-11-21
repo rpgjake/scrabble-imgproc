fcd09df2f1638e46c75b93cfd7b3831735c98003
Stephen Wilkins <wilkins.c.stephen@gmail.com>
2018-11-20 21:56:57 -0500

    Created a basic grid for extracting tiles

--- Scrabble.py
+++ Scrabble.py
@@ -1,55 +1,29 @@
 import dippykit as dip
 import numpy as np
-import skimage.morphology as morph
 import scipy.ndimage as ndImage
 import skimage.transform as tf
 import cv2
 
-image = dip.im_read('images/Words6.jpg')
+image = dip.im_read('images/Words4.jpg')
 image = (image[:, :, 2])
 
 x, y = image.shape
-if max(x, y) > 1000:
-    scale = 1000.0 / max(x, y)
+if min(x, y) > 1000:
+    scale = 1000.0 / min(x, y)
     newSize = (int(scale * y), int(scale * x))
     image = dip.resize(image, newSize)
 
-image2 = np.zeros(image.shape, dtype=np.uint8)
 original = image
 
-# conn = np.array([[0, 0, 0, 1, 1, 1, 0, 0, 0],
-#                  [0, 0, 0, 1, 1, 1, 0, 0, 0],
-#                  [0, 0, 0, 1, 1, 1, 0, 0, 0],
-#                  [1, 1, 1, 1, 1, 1, 1, 1, 1],
-#                  [1, 1, 1, 1, 1, 1, 1, 1, 1],
-#                  [1, 1, 1, 1, 1, 1, 1, 1, 1],
-#                  [0, 0, 0, 1, 1, 1, 0, 0, 0],
-#                  [0, 0, 0, 1, 1, 1, 0, 0, 0],
-#                  [0, 0, 0, 1, 1, 1, 0, 0, 0]])
-# conn = np.array([[1, 1],
-#                  [1, 1]])
-conn = np.array([[0, 1, 0],
-                 [1, 1, 1],
-                 [0, 1, 0]])
-# conn = np.array([[0, 0, 1, 1, 0, 0],
-#                  [0, 0, 1, 1, 0, 0],
-#                  [1, 1, 1, 1, 1, 1],
-#                  [1, 1, 1, 1, 1, 1],
-#                  [0, 0, 1, 1, 0, 0],
-#                  [0, 0, 1, 1, 0, 0]])
-
-
-
-
-
+# Image2 draws the lines that are detected
+image2 = np.zeros(image.shape, dtype=np.uint8)
 lsd = cv2.createLineSegmentDetector(0)
 lines = lsd.detect(image)[0]
 for line in lines:
     for (x1, y1, x2, y2) in line:
         length = abs(np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
-        cv2.line(image2, (x1, y1), (x2, y2), 255, 1)
+        cv2.line(image2, (x1, y1), (x2, y2), 255, 3)
 image2Lines = image2
-image2 = morph.dilation(image2, selem=conn)
 
 Labeled, numobj = ndImage.label(image2)
 lastSum = 0
@@ -60,9 +34,6 @@ for item in range(1, numobj + 1):
     if newSum > lastSum:
         displayImage = newImage
         lastSum = newSum
-
-# dip.figure("Largest Blob")
-# dip.imshow(displayImage, 'gray')
 
 cornerBR = (0, 0)
 sumBR = 0
@@ -92,10 +63,6 @@ for x in range(0, imagex):
             if temp > sumBL:
                 sumBL = temp
                 cornerBL = (x, y)
-print("TL:  " + str(cornerTL))
-print("TR:  " + str(cornerTR))
-print("BL:  " + str(cornerBL))
-print("BR:  " + str(cornerBR))
 dest = np.array([cornerTL, cornerBL, cornerBR, cornerTR])
 scale = 1000 # 15 * 15 * 20
 src = np.array([[0, 0], [0, scale], [scale, scale], [scale, 0]])
@@ -106,7 +73,7 @@ warped = warped[30 : -30, 30 : -30]
 
 dip.figure("warped")
 dip.imshow(warped, 'gray')
-dip.show()
+# dip.show()
 
 
 warped = dip.float_to_im(warped)
@@ -117,8 +84,6 @@ for line in lines:
     for (x1, y1, x2, y2) in line:
         length = abs(np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
         cv2.line(image3, (x1, y1), (x2, y2), 255, 3)
-# image3 = morph.dilation(image3, selem=conn)
-
 
 Labeled, numobj = ndImage.label(image3)
 lastSum = 0
@@ -133,10 +98,101 @@ for item in range(1, numobj + 1):
 
 dip.figure("Display image")
 dip.imshow(displayImage, 'gray')
-dip.show()
+# dip.show()
 
+dimX, dimY = warped.shape
 
+topRow = 0
+botRow = dimY - 1
+leftCol = 0
+rightCol = dimX - 1
+while np.amax(displayImage[:, leftCol]) == 0:
+    leftCol += 1
+while np.amax(displayImage[:, rightCol]) == 0:
+    rightCol -= 1
+while np.amax(displayImage[topRow]) == 0:
+    topRow += 1
+while np.amax(displayImage[botRow]) == 0:
+    botRow -= 1
 
-dip.figure("Image 3")
-dip.imshow(image3, 'gray')
+lineTop = (topRow, topRow)
+lineBot = (botRow, botRow)
+lineLeft = (leftCol, leftCol)
+lineRight = (rightCol, rightCol)
+bestScoreTop = 0
+bestScoreBot = 0
+bestScoreLeft = 0
+bestScoreRight = 0
+
+canvas = np.zeros(warped.shape, dtype=np.uint8)
+for i in range(6, 50):
+    for j in range(6, 50):
+        # Top Row
+        canvas.fill(0)
+        x1 = 0
+        y1 = topRow + i
+        x2 = dimX - 1
+        y2 = topRow + j
+        cv2.line(canvas, (x1, y1), (x2, y2), 255, 10)
+        score = np.count_nonzero(np.logical_and(canvas > 0, displayImage))
+        if score > bestScoreTop:
+            lineTop = (y1, y2)
+            bestScoreTop = score
+
+        # Bottom Row
+        canvas.fill(0)
+        x1 = 0
+        y1 = botRow - i
+        x2 = dimX - 1
+        y2 = botRow - j
+        cv2.line(canvas, (x1, y1), (x2, y2), 255, 10)
+        score = np.count_nonzero(np.logical_and(canvas > 0, displayImage))
+        if score > bestScoreBot:
+            lineBot = (y1, y2)
+            bestScoreBot = score
+
+        # Left Column
+        canvas.fill(0)
+        x1 = leftCol + i
+        y1 = 0
+        x2 = leftCol + j
+        y2 = dimY - 1
+        cv2.line(canvas, (x1, y1), (x2, y2), 255, 10)
+        score = np.count_nonzero(np.logical_and(canvas > 0, displayImage))
+        if score > bestScoreLeft:
+            lineLeft = (x1, x2)
+            bestScoreLeft = score
+
+        # Right Column
+        canvas.fill(0)
+        x1 = rightCol - i
+        y1 = 0
+        x2 = rightCol - j
+        y2 = dimY - 1
+        cv2.line(canvas, (x1, y1), (x2, y2), 255, 10)
+        score = np.count_nonzero(np.logical_and(canvas > 0, displayImage))
+        if score > bestScoreRight:
+            lineRight = (x1, x2)
+            bestScoreRight = score
+
+cv2.line(warped, (0, lineTop[0]), (dimY - 1, lineTop[1]), 0, 3)
+cv2.line(warped, (0, lineBot[0]), (dimY - 1, lineBot[1]), 0, 3)
+xDiff0 = (lineBot[0] - lineTop[0]) / 15.0
+xDiff1 = (lineBot[1] - lineTop[1]) / 15.0
+yDiff0 = (lineRight[0] - lineLeft[0]) / 15.0
+yDiff1 = (lineRight[1] - lineLeft[1]) / 15.0
+for i in range(1, 15):
+    y1 = int(lineTop[0] + i * xDiff0)
+    y2 = int(lineTop[1] + i * xDiff1)
+    cv2.line(warped, (0, y1), (dimY - 1, y2), 0, 3)
+
+    x1 = int(lineLeft[0] + i * yDiff0)
+    x2 = int(lineLeft[1] + i * yDiff1)
+    cv2.line(warped, (x1, 0), (x2, dimX - 1), 0, 3)
+
+cv2.line(warped, (lineLeft[0], 0), (lineLeft[1], dimX - 1), 0, 3)
+cv2.line(warped, (lineRight[0], 0), (lineRight[1], dimX - 1), 0, 3)
+
+dip.figure("Lined image")
+dip.imshow(warped, 'gray')
 dip.show()
